c90d5861408773254d24c3c4344167992126be96
manuwwa <manuwwa@gmail.com>
2020-11-07 20:10:01 +0100

    Fix hand movement

--- src/main/java/com/manuwwaMods/komputerdoprzepraszania/events/ModClientEvents.java
+++ src/main/java/com/manuwwaMods/komputerdoprzepraszania/events/ModClientEvents.java
@@ -3,8 +3,8 @@ package com.manuwwaMods.komputerdoprzepraszania.events;
 import com.manuwwaMods.komputerdoprzepraszania.komputerdoprzepraszania;
 import com.manuwwaMods.komputerdoprzepraszania.util.RegistryHandler;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
@@ -28,14 +28,13 @@ public class ModClientEvents {
         komputerdoprzepraszania.LOGGER.info(item.getItem().getDisplayName(item));
         if(isKOMPUTER_DO_PRZEPRASZANIA_BLOCK && item == ItemStack.EMPTY)
         {
-                //#todo add hand movement
                 ResourceLocation location = new ResourceLocation(komputerdoprzepraszania.MOD_ID, "block.komputer_do_przepraszania_block.voice");
                 SoundEvent event = new SoundEvent(location);
                 double x = ev.getPos().getX();
                 double y = ev.getPos().getY();
                 double z = ev.getPos().getZ();
                 world.playSound(x, y, z, event, SoundCategory.VOICE, 1.0f, 1.0f, false);
+                player.swing(Hand.MAIN_HAND,true);
         }
-
     }
 }
